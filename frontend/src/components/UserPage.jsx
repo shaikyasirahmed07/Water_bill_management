b12769e62d591c05831ed700ae120c6f75a9370f
YASIR AHMED SHAIK <147685813+shaikyasirahmed07@users.noreply.github.com>
2025-04-14 17:09:53 +0530

    Update UserPage.jsx

--- frontend/src/components/UserPage.jsx
+++ frontend/src/components/UserPage.jsx
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import contractJson from "../artifacts/contracts/WaterBill.sol/WaterBill.json";
 import '../App.css';
 
-const contractAddress = "0x2a25F6402d4c7BD99a7E07706fDc142fA5581797"; // ✅ Update this if needed
+const contractAddress = "0xC94894Ca2B21051ceE68409387c34Cb885d3c66E"; // ✅ Update this if needed
 const contractABI = contractJson.abi;
 
 const UserPage = () => {
@@ -56,16 +56,9 @@ const UserPage = () => {
             return;
         }
 
-        const billInWei = ethers.parseEther(userBill.amount); // Get the bill amount in Wei
-
-        if (ethers.parseEther(billAmount).lt(billInWei)) {
-            alert("Insufficient payment. Please enter the correct amount.");
-            return;
-        }
-
         try {
             const tx = await contract.payBill({
-                value: billInWei // Ensure to pay the exact amount (in wei)
+                value: ethers.parseEther(billAmount) // Convert ETH to wei
             });
             await tx.wait();
             alert("✅ Bill paid!");
