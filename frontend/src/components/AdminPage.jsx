33184db3a6f64f843a21268d5f0b8af431b7afa2
YASIR AHMED SHAIK <147685813+shaikyasirahmed07@users.noreply.github.com>
2025-04-14 16:48:15 +0530

    Update AdminPage.jsx

--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import contractJson from "../artifacts/contracts/WaterBill.sol/WaterBill.json";
 import '../App.css';
 
-const contractAddress = "0x5BE45A2f9Bdec87447f73b9847A0E3AFdD3e9791";
+const contractAddress = "0xC94894Ca2B21051ceE68409387c34Cb885d3c66E";
 const contractABI = contractJson.abi;
 
 const AdminPage = () => {
