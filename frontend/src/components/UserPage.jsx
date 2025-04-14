dec79bb42470a87da2e59bfd891eeccdacadd7c6
YASIR AHMED SHAIK <147685813+shaikyasirahmed07@users.noreply.github.com>
2025-04-14 17:10:15 +0530

    Update UserPage.jsx

--- frontend/src/components/UserPage.jsx
+++ frontend/src/components/UserPage.jsx
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import contractJson from "../artifacts/contracts/WaterBill.sol/WaterBill.json";
 import '../App.css';
 
-const contractAddress = "0xC94894Ca2B21051ceE68409387c34Cb885d3c66E"; // ✅ Update this if needed
+const contractAddress = "0x24CF1CF3db0BE4a33f4e32935A917d49B2d73e3b"; // ✅ Update this if needed
 const contractABI = contractJson.abi;
 
 const UserPage = () => {
